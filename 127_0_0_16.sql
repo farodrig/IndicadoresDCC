358e0c9634898ceb9fe682c368a5780798c63242
Agustin Lopez <lopezmen@hotmail.com>
2015-05-25 17:02:03 -0300

    se agrega ultima bd a git

--- 127_0_0_16.sql
+++ 127_0_0_16.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2015 a las 21:11:14
+-- Tiempo de generación: 22-05-2015 a las 20:57:25
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `dashboard` (
 `id` int(11) NOT NULL,
   `org` int(11) NOT NULL,
   `title` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `dashboard`
@@ -59,7 +59,9 @@ INSERT INTO `dashboard` (`id`, `org`, `title`) VALUES
 (1, 2, 'Dashboard Pregrado'),
 (6, 3, 'Dashboard docencia'),
 (7, 1, 'Dashboard DCC'),
-(8, 7, 'Dashboard Que tal');
+(9, 21, 'Dashboard Que tal'),
+(10, 0, 'Dashboard DCC'),
+(11, 7, 'Dashboard Postgrado');
 
 -- --------------------------------------------------------
 
@@ -71,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `graphdash` (
 `id` int(11) NOT NULL,
   `dashboard` int(11) NOT NULL,
   `graphic` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `graphdash`
@@ -82,12 +84,16 @@ INSERT INTO `graphdash` (`id`, `dashboard`, `graphic`) VALUES
 (2, 1, 2),
 (5, 1, 11),
 (8, 1, 14),
+(19, 1, 25),
+(20, 1, 26),
 (3, 6, 9),
 (6, 6, 12),
 (7, 7, 13),
 (9, 7, 15),
 (11, 7, 16),
-(10, 8, 16);
+(15, 7, 21),
+(17, 9, 17),
+(18, 11, 24);
 
 -- --------------------------------------------------------
 
@@ -102,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `graphic` (
   `min_year` int(11) NOT NULL,
   `max_year` int(11) NOT NULL,
   `position` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `graphic`
@@ -112,13 +118,22 @@ INSERT INTO `graphic` (`id`, `type`, `metorg`, `min_year`, `max_year`, `position
 (1, 2, 1, 2013, 2015, 1),
 (2, 1, 2, 2013, 2015, 0),
 (9, 1, 5, 2000, 2015, 0),
-(11, 2, 5, 2008, 2015, 1),
+(11, 2, 5, 2008, 2014, 1),
 (12, 1, 14, 2000, 2015, 1),
-(13, 1, 15, 2006, 2015, 0),
+(13, 1, 15, 2006, 2015, 1),
 (14, 2, 14, 2000, 2026, 1),
 (15, 1, 5, 2015, 2015, 1),
-(16, 2, 19, 2005, 2015, 1),
-(17, 2, 19, 2005, 2015, 1);
+(16, 2, 19, 2005, 2008, 1),
+(17, 2, 19, 2005, 2015, 1),
+(18, 2, 19, 2005, 2015, 1),
+(19, 2, 17, 2005, 2015, 1),
+(20, 2, 17, 2005, 2015, 1),
+(21, 2, 12, 2005, 2015, 1),
+(22, 2, 17, 2005, 2015, 1),
+(23, 2, 19, 2005, 2015, 1),
+(24, 1, 19, 2005, 2015, 0),
+(25, 2, 6, 2005, 2015, 0),
+(26, 2, 12, 2005, 2015, 0);
 
 -- --------------------------------------------------------
 
@@ -157,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `measure` (
   `dateup` datetime DEFAULT NULL,
   `validator` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `dateval` datetime DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `measure`
@@ -173,13 +188,14 @@ INSERT INTO `measure` (`id`, `metorg`, `state`, `value`, `target`, `expected`, `
 (26, 2, 1, 20, 21, 22, 2013, '18.292.316-8', '2015-05-10 11:52:20', NULL, NULL),
 (27, 2, 1, 15, 20, 0, 2015, '18.292.316-8', '2015-05-10 11:25:57', NULL, NULL),
 (30, 5, 0, 15, 25, 30, 2000, '20.584.236-5', '2015-05-20 20:36:00', NULL, NULL),
-(33, 5, 1, 200, 200, 200, 2015, '18.292.316-8', '2015-05-11 10:29:45', NULL, NULL),
+(33, 5, 1, 200, 200, 200, 2015, '17.586.757-0', '2015-05-21 17:07:17', NULL, NULL),
 (34, 5, 0, 200, 200, 200, 2014, '18.292.316-8', '2015-05-11 10:30:06', NULL, NULL),
 (35, 5, 1, 20, 10, 20, 2013, '18.292.316-8', '2015-05-13 00:02:03', NULL, NULL),
 (36, 14, 1, 15, 20, 30, 2000, '18.292.316-8', '2015-05-17 20:12:55', NULL, NULL),
 (37, 1, 1, 15, 20, 25, 2016, '18.292.316-8', '2015-05-20 14:53:49', NULL, NULL),
 (38, 2, 1, 23, 23, 25, 2016, '18.292.316-8', '2015-05-20 14:53:49', NULL, NULL),
-(39, 16, 1, 0, 20, 15, 2016, '18.292.316-8', '2015-05-20 14:54:15', NULL, NULL);
+(39, 16, 1, 0, 20, 15, 2016, '18.292.316-8', '2015-05-20 14:54:15', NULL, NULL),
+(40, 16, 1, 20, 20, 20, 2015, '18.292.316-8', '2015-05-21 17:53:27', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -310,7 +326,7 @@ CREATE TABLE IF NOT EXISTS `permits` (
 --
 
 INSERT INTO `permits` (`user`, `director`, `visualizer`, `assistant_unidad`, `in_charge_unidad`, `finances_assistant_unidad`, `dcc_assistant`) VALUES
-('17.586.757-0', 0, 1, '-1', '-1', '-1', 0),
+('17.586.757-0', 0, 1, '-1', '3', '-1', 0),
 ('18.292.316-8', 1, 0, '-1', '-1', '-1', 0),
 ('20.584.236-5', 0, 1, '3 4', '-1', '-1', 0);
 
@@ -487,17 +503,17 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Categorías en las que se a
 -- AUTO_INCREMENT de la tabla `dashboard`
 --
 ALTER TABLE `dashboard`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT de la tabla `graphdash`
 --
 ALTER TABLE `graphdash`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT de la tabla `graphic`
 --
 ALTER TABLE `graphic`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=18;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT de la tabla `graphtype`
 --
@@ -507,7 +523,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- AUTO_INCREMENT de la tabla `measure`
 --
 ALTER TABLE `measure`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=40;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
 --
 -- AUTO_INCREMENT de la tabla `metorg`
 --
