4219fe68f1ee28e99ccbb1a0a2fb1b934f4e2607
Felipe Rodríguez <farodrig92@gmail.com>
2015-05-28 00:45:37 -0300

    Arreglado problema con visualización de navegador izquierdo

--- application/views/noDashboard.php
+++ application/views/noDashboard.php
@@ -59,9 +59,8 @@
 							<i class="fa fa-bars" aria-label="Toggle sidebar"></i>
 						</div>
 					</div>
-
-					<div class="nano">
-						<div class="nano-content">
+					<div class="nano has-scrollbar">
+						<div class="nano-content" tabindex="0" style="right: -12px;">
 							<nav id="menu" class="nav-main" role="navigation">
 								<ul class="nav nav-main">
 									<li>
@@ -73,7 +72,9 @@
 								</ul>
 							</nav>
 						</div>
-
+                        <div class="nano-pane" style="display: none; opacity: 1; visibility: visible;">
+                            <div class="nano-slider" style="height: 781px; transform: translate(0px, 0px);"></div>
+                        </div>
 					</div>
 
 				</aside>
