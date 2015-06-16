a71a513a67924b5fe3a58e28d9bfe9c23552b8b8
Camila Alvarez <cam.alvarez.i@gmail.com>
2015-06-15 21:59:51 -0300

    recupero el modelo perdido

--- application/models/Metorg_model.php
+++ application/models/Metorg_model.php
@@ -1,10 +1,7 @@
 <?php
 class Metorg_model extends CI_Model{
-
-
 	function addMetOrg($data){
 		//REcuerda insertar en MetOrg
-		return $this->db->insert('MetOrg', $data); 
-
+		return $this->db->insert('MetOrg', $data);
 	}
 }
